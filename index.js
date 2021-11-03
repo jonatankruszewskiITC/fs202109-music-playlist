9874982cf8840835f7102c39ce367406469d780b
Aviad Shmuel <vocaviad@Aviads-MacBook-Pro.local>
2021-11-03 13:29:26 +0200

    nono this is the best

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const playlist = [
     { title: "I was made for loving you", band: "Kiss" },
     { title: "Born dead", band: "Death" },
     { title: "Derech Hashalom", band: "Peer Tasi" }
+    { title: "Coco Jambo", band: "Mr President" }
 ];
