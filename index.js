57cdf4795439ed6c4f686eb728970ae6b0528a4d
FrischeD <david.frischer@unifr.ch>
2021-11-03 13:37:01 +0200

    adding Californication

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const playlist = [
     { title: "I was made for loving you", band: "Kiss" },
     { title: "Born dead", band: "Death" },
-    { title: "Derech Hashalom", band: "Peer Tasi" }
-    { title: "Coco Jambo", band: "Mr President" }
+    { title: "Derech Hashalom", band: "Peer Tasi" },
+    { title: "Coco Jambo", band: "Mr President" },
     { title: "Sweet child of mine", band: "Gun's and roses" },
     { title: "Californication", band: "Red Hot Chilli peppers" }
 ];
