b136025fb27cfdaf27e909ed79a35770083d55a1
jonatan <jonatan.kruszewski@itc.tech>
2021-11-03 13:29:19 +0200

    Added Sweet child of mine

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const playlist = [
     { title: "I was made for loving you", band: "Kiss" },
     { title: "Born dead", band: "Death" },
-    { title: "Derech Hashalom", band: "Peer Tasi" }
+    { title: "Derech Hashalom", band: "Peer Tasi" },
+    { title: "Sweet child of mine", band: "Gun's and roses" }
 ];
