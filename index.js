1a7be59daa56a9a0eae23a14b493b49e6c4da066
Ourcodingadventure <Javitan0309@gmail.com>
2021-11-03 13:37:03 +0200

    hi im here to make you have a bad time

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const playlist = [
-    { title: "I was made for loving you", band: "Kiss" },
-    { title: "Born dead", band: "Death" },
-    { title: "Derech Hashalom", band: "Peer Tasi" }
-    { title: "Coco Jambo", band: "Mr President" }
-    { title: "Sweet child of mine", band: "Gun's and roses" }
+  { title: "I was made for loving you", band: "Kiss" },
+  { title: "Born dead", band: "Death" },
+  { title: "Derech Hashalom", band: "Peer Tasi" },
+  { title: "Coco Jambo", band: "Mr President" },
+  { title: "Sweet child of mine", band: "Gun's and roses" },
+  { title: `Africa`, band: `Toto` },
 ];
